da020625758402f9774fceedab4f99974469900e
a.skupov <a.skupov@bio-smart.ru>
2023-05-29 18:31:21 +0500

    My first commit

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,25 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
 // Напишите ответ здесь:
 
+#include <cstring>
+
+int how_much_three(void)
+{
+    int count = 0;
+    std::string str;
+
+    for(int i = 1; i < 1000; ++i)
+    {
+        for( const auto &i : std::to_string(i) )
+        {
+            if( i == '3')
+            {
+                ++count;
+                break;
+            }
+        }
+    }
+
+    return count;
+}
 // Закомитьте изменения и отправьте их в свой репозиторий.
